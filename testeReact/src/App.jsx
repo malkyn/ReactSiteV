e270127d57b94d579202790d91693bbc4242a280
vinir <46539805+malkyn@users.noreply.github.com>
2024-05-23 17:40:17 -0300

    PropClass

--- testeReact/src/App.jsx
+++ testeReact/src/App.jsx
@@ -1,13 +1,20 @@
+/* eslint-disable no-unused-vars */
 //External é bom para estilos globais
 //Modules é bom para components com mais estilizações
 //Inline é bom para componentes menores com pouco estilo
 
+
+import Students from './components/Students/Students';
 import Button from './Button'
 function App() {
  
   return(
     <>
-      <Button></Button>
+      <Students name="SpongeBob" age={30}isStudent={true}></Students>
+      <Students name="Patrick" age={42}isStudent={false}></Students>
+      <Students name="Squidward" age={50}isStudent={false}></Students>
+      <Students name="Sandy" age={27}isStudent={true}></Students>
+      <Students></Students>
     </>
   );
 }
