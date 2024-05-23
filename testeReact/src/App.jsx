20b7ba922f6e4f7db462c325c42595cc14bbebff
vinir <46539805+malkyn@users.noreply.github.com>
2024-05-23 17:23:37 -0300

    Botão

--- testeReact/src/App.jsx
+++ testeReact/src/App.jsx
@@ -1,12 +1,13 @@
-import Card from './components/Card'
+//External é bom para estilos globais
+//Modules é bom para components com mais estilizações
+//Inline é bom para componentes menores com pouco estilo
 
+import Button from './Button'
 function App() {
  
   return(
     <>
-      <Card/>
-      <Card/>
-      <Card/>
+      <Button></Button>
     </>
   );
 }
